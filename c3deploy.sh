176facb57f6db3e01a4f0bc99d8ed8f41bfa8809
joey-cliqr <jomerz@cisco.com>
2018-05-14 13:32:14 -0700

    c3deploy with test name

--- c3deploy.sh
+++ c3deploy.sh
@@ -10,7 +10,7 @@ curl -k -X POST \
   -d '{
         "appId": "12616",
         "appVersion": "1",
-        "name": "Pipeline_$JOB_NAME_$BUILD_NUMBER",
+        "name": "Pipeline_Test001",
         "metadatas": [
                 {
                         "namespace": "",
