746fa14111a1af1f7f4f3d93b4cb7d0a5a080158
joey-cliqr <jomerz@cisco.com>
2018-05-15 07:32:10 -0700

    update

--- c3deploy.sh
+++ c3deploy.sh
@@ -11,7 +11,7 @@ curl -k -X POST \
   -d '{
         "appId": "12616",
         "appVersion": "1",
-        "name": "$1",
+        "name": "'$1'",
         "metadatas": [
                 {
                         "namespace": "",
